4a8ce4af043bf1d35cda3ae2a662a78594250f47
sperrongit <100207565+sperrongit@users.noreply.github.com>
2022-02-22 12:06:02 -0500

    Update main.tf

--- lab-1-terraform-resources/terraform/resource-group/main.tf
+++ lab-1-terraform-resources/terraform/resource-group/main.tf
@@ -3,8 +3,13 @@ resource "random_uuid" "uuid" {}
 resource "azurerm_resource_group" "rg" {
 name = "rg-devops-tf-${random_uuid.uuid.result}"
 location = var.location
-}
 
 tags = {
 environment = "dev"
+}
+
+{
+provider "azurerm" {
+# Configuration options
+features {}
 }
