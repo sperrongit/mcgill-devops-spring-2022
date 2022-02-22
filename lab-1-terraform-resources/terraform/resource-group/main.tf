668225201b257b3aff01d2d9cc9b66f7f96d3d3f
sperrongit <sebastien.perron@fxinnovation.com>
2022-02-22 15:07:43 +0000

    Adding Environment Tag for Compliance Checks

--- lab-1-terraform-resources/terraform/resource-group/main.tf
+++ lab-1-terraform-resources/terraform/resource-group/main.tf
@@ -4,12 +4,6 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-devops-tf-${random_uuid.uuid.result}"
   location = var.location
 
-
-}
-provider "azurerm" {
-  # Configuration options
-  features {}
-
 }
   tags = {
     environment = "dev"
